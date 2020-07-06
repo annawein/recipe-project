fa3fd15f746c5bf91ac512538304cf7050bd518c
Anna Weingart <annaraewein@gmail.com>
2020-07-06 16:37:16 +0200

    created sign in, log in, log out

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const ensureLogin = require("connect-ensure-login");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -30,6 +31,11 @@ router.post("/signup", (req, res, next) => {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
   }
+
+  router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render("private", { user: req.user });
+  });
+
 
   User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
