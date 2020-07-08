190dcb100bdadd864d9a163cb4f6d72aff8cb31a
vmayrock <verena.mayrock@gmail.com>
2020-07-08 10:41:15 +0200

    shopping list adding works

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,41 @@ const express = require('express');
 const Recipe = require('../models/Recipe');
 const User = require('../models/User');
 const router  = express.Router();
+const ensureLogin = require("connect-ensure-login");
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  console.log("this is the user?", req.user)
-  const user = req.user;
-  Recipe.find().then(recipesFromDB => {
-    res.render('index', {recipeList: recipesFromDB, user: user});
-  })
-  console.log(user);
+router.get("/", (req, res, next) => {
+  res.render("auth/login");
 });
 
-router.get('/addRecipe', (req, res) => {
+
+// router.get('/', (req, res, next) => {
+//   console.log("this is the user?", req.user)
+//   const user = req.user;
+//   Recipe.find().then(recipesFromDB => {
+//     res.render('index', {recipeList: recipesFromDB, user: user});
+//   })
+//   console.log(user);
+// });
+
+//get to the all recipes dashboard
+router.get("/allRecipes", ensureLogin.ensureLoggedIn(), (req, res) => {
+  console.log(req.user); 
+  const user = req.user;
+  Recipe.find()
+  .then(recipesFromDB => {
+    res.render('allRecipes', {recipeList: recipesFromDB, user: user});
+  })
+  .catch((err)=> {
+    console.log(err); 
+    next(); 
+  }); 
+}); 
+
+
+
+router.get('/addRecipe', ensureLogin.ensureLoggedIn(), (req, res) => {
   User.find().then(usersFromDB => {
     res.render('addRecipe', {users: usersFromDB})
   }).catch(err => {
@@ -33,26 +56,26 @@ router.post('/addRecipe', (req, res) => {
     name: name, 
   }).then(recipe => {
     console.log(`Success,${title} was added to the database.`); 
-    res.redirect('/'); 
+    res.redirect('/allRecipes'); 
   }).catch(err => {
     console.log(err); 
   })
 }); 
 
 //see shopping list for chosen recipe, imported from Annas code
-router.get("/shopping-list", (req, res) => {
-  const user = req.user;
-  console.log(req.user)
-  if(req.user){res.render("shopping-list",{user});}
-  else{  res.redirect("/auth/login")}
-});
+// router.get("/shopping-list", (req, res) => {
+//   const user = req.user;
+//   console.log(req.user)
+//   if(req.user){res.render("shopping-list",{user});}
+//   else{  res.redirect("/auth/login")}
+// });
 
-router.get('/:recipeId', (req, res, next) => {
+router.get('/allRecipes/:recipeId', (req, res, next) => {
   const recipeId = req.params.recipeId; 
   const user=req.user; 
   Recipe.findById(recipeId)
   .then(recipe => {
-    console.log(recipe); 
+    // console.log(recipe); 
     res.render('recipeDetails', {recipe: recipe, user}); 
   }).catch(err => {
     console.log(err); 
--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,29 @@ const router = express.Router();
 const User = require('../models/User'); 
 const Recipe = require('../models/Recipe'); 
 const mongoose = require('mongoose'); 
+const ensureLogin = require("connect-ensure-login");
 
+//new
+router.get("/shoppingList", ensureLogin.ensureLoggedIn(), (req, res) => {
+  console.log(req.user); 
+  const user = req.user;
+  Recipe.find()
+  .then(recipesFromDB => {
+    console.log("do I even get here?", recipesFromDB)
+    res.render('shopping-list', {list: recipesFromDB, user: user});
+  })
+  .catch((err)=> {
+    console.log(err); 
+    next(); 
+  }); 
+}); 
+
+router.post("/shoppingList", (req, res) => {
+  console.log(req.body.ingredient)
+  let ing= req.body
+  console.log(Object.values(ing))
+  res.render('shopping-list',{ing})
+})
 
 router.post('/addShoppingList/:id', (req, res, next) => {
   console.log("working", req.params.id,req.user); 
@@ -14,6 +36,8 @@ router.post('/addShoppingList/:id', (req, res, next) => {
 }).catch(error=>console.log(error))
 }); 
 
+
+//this gets called with the button
 router.get('/add/shoppingList/:id', (req, res, next) => {
   console.log("working?",req.params.id)
   res.render('shopping-list')
