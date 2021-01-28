301282a81cf938f25984ea1a774b3161b08a73d3
Anna Weingart <annaraewein@gmail.com>
2021-01-28 13:01:19 +0100

    modified mongodb atlas connection string

--- bin/seeds.js
+++ bin/seeds.js
@@ -9,7 +9,7 @@ const Recipe = require('../models/Recipe');
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://annawein:annawein@cluster0.mji2z.mongodb.net/kitchen-hack?retryWrites=true&w=majority";
+const uri = "mongodb+srv://annabanana:annabanana@cluster0.mji2z.mongodb.net/kitchen-hack?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
